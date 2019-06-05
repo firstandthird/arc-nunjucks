581f9842789658d6d45e4d6d16da246f070ce0f8
orthagonal <orthagonal1@gmail.com>
2019-06-05 01:07:27 +0000

    load filters from /helpers

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const arc = require('@architect/functions');
 const static = arc.http.helpers.static;
 const mapping = require('@architect/shared/assets.json');
 const path = require('path');
+const fs = require('fs');
 
 const routeDir = path.resolve(__dirname, '../../../');
 const viewDir = path.resolve(routeDir, 'node_modules/@architect/views');
 
 const nEnv = nunjucks.configure([routeDir, viewDir], { autoescape: true });
+
+if (fs.existsSync(`${viewDir}/helpers`)) {
+  fs.readdirSync(`${viewDir}/helpers`).forEach(helperFile => {
+    const func = require(`${viewDir}/helpers/${helperFile}`);
+    const name = path.basename(helperFile, '.js');
+    nEnv.addFilter(name, func);
+  });
+}
 
 const asset = function (file) {
   if (mapping && mapping[file]) {
