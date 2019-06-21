4d2e9e0685a27e2876caca31ddfe8ee8c3f97aec
orthagonal <orthagonal1@gmail.com>
2019-06-21 04:53:18 +0000

    add tests

---
+++ test/test.views.js
@@ -0,0 +1,26 @@
+const tap = require('tap');
+const arc = require('@architect/functions');
+// stub static function:
+arc.http.helpers.static = (myAsset) => myAsset;
+
+process.env.VIEWS_PATH = `${__dirname}/views`;
+process.env.SHARED_PATH = __dirname;
+const aj = require('../');
+
+tap.test('renders views', async t => {
+  const r = await aj.render('hithere.njk', { name: 'robert' });
+  t.match(r, 'hi there robert');
+  t.end();
+});
+
+tap.test('registers and uses helpers', async t => {
+  const r = await aj.render('shorten.njk', { name: 'robert' });
+  t.match(r, 'hi there rob');
+  t.end();
+});
+
+tap.test('provides static assets', async t => {
+  const r = await aj.render('assets.njk', { name: 'robert' });
+  t.match(r, 'hi there /_dist/mappedAsset.jpeg');
+  t.end();
+});
